381a118fac7cab884e704f2c72295db9f4259df1
daniel ikokoh <danielikokoh@MACs-MBP-3.ipnxng.net>
2023-01-30 12:42:26 +0100

    seperating vue components into a javascript file and importing it

---
+++ js/components/AppButtons.js
@@ -0,0 +1,13 @@
+export default {
+    template: `
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" :disabled="processing">
+            <slot />
+        </button>
+    `,
+    
+    data() {
+        return {
+            processing: false
+        };
+    }
+}
