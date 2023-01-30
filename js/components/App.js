d59951b0d437435bdd56e0b2b1a33945407ca488
daniel ikokoh <danielikokoh@MACs-MBP-3.ipnxng.net>
2023-01-30 13:31:07 +0100

    how to pass props in to components

---
+++ js/components/App.js
@@ -0,0 +1,9 @@
+import AppButton from './AppButtons.js'
+
+export default {
+        components: {
+            'app-button': AppButton,
+
+        }
+
+}
--- js/components/AppButtons.js
+++ js/components/AppButtons.js
@@ -1,13 +1,23 @@
 export default {
     template: `
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" :disabled="processing">
+        <button :class="{ 
+            'text-white font-bold py-2 px-4 rounded': true, 
+            'bg-blue-500 hover:bg-blue-700' : type === 'primary',
+            'bg-green-500 hover:bg-green-700' : type === 'secondary',
+            'bg-gray-500 hover:bg-gray-700' : type === 'muted',
+            'is-loading': processing,
+            }" :disabled="processing">
             <slot />
         </button>
     `,
-    
-    data() {
-        return {
-            processing: false
-        };
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing: {
+            type: Boolean,
+            default: false
+        }
     }
 }
